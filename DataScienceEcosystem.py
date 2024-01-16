7c1e5fbbb98e5c9b7625b367859906e42b949855
TUSHAR ATKARE <tusharatkare06@gmail.com>
2024-01-16 19:42:11 +0530

    1-Notebook

---
+++ DataScienceEcosystem.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Data Science Tools and Ecosystem 
+
+# ### Below are a few examples of evaluating arithmetic expressions in Python
+
+# In[2]:
+
+
+(3*4)+5
+#This a simple arithmetic expression to mutiply then add integers
+
+
+# In[3]:
+
+
+# This will convert 200 minutes to hours by dividing by 60.
+minutes = 200
+hours = minutes / 60
+hours
+
+
+# **Objectives:**
+# 
+# - List popular languages for Data Science.
+# - Introduce commonly used libraries by Data Scientists.
+# - Discuss the importance of version control and GitHub in collaborative projects.
+# - Demonstrate working with Jupyter Notebooks and RStudio.
+# - Highlight key concepts in data visualization and analysis tools.
+# 
+
+# ## Author
+# 
+# name (Tushar Atkare)
+
+# In[ ]:
+
+
+
+
